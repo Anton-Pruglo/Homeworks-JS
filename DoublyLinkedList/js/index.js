2358760e5d67a9f3a994ea70b77266e93e21a974
Anton-Pruglo <xatastudio@gmail.com>
2021-04-08 17:07:23 +0300

    Redact

--- DoublyLinkedList/js/index.js
+++ DoublyLinkedList/js/index.js
@@ -13,21 +13,90 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+    pop() {
+        if (this.length === 0) return undefined;
+        const temp = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = temp.prev;
+            this.tail.next = null;
+            temp.prev = null;
+        }
+        this.length--;
+        return temp;
+        }
 
+     push(val) {
+            const newNode = new DoublyLinkedListNode(val);
+            if (this.length === 0) {
+                this.head = newNode;
+                this.tail = newNode;
+            } else {
+                this.tail.next = newNode;
+                newNode.prev = this.tail;
+                this.tail = newNode;
+            }
+            this.length++;
+            return this;
+    }
 
- push(val) {
+    shift() {
+        if (this.length === 0) return undefined;
+        const temp = this.head;
+        if (this.length === 1) {
+             this.head = null;
+             this.tail = null;
+        } else {
+            this.head = temp.next;
+            this.head.prev = null;
+            temp.next = null;
+        }
+        this.length--;
+        return temp;
+        }
+
+    unshift(val) {
         const newNode = new DoublyLinkedListNode(val);
         if (this.length === 0) {
             this.head = newNode;
             this.tail = newNode;
         } else {
-            this.tail.next = newNode;
-            newNode.prev = this.tail;
-            this.tail = newNode;
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = newNode;
         }
         this.length++;
         return this;
-}
+    }
+
+    toArray() {
+        const listNode = [];
+        let currentNode = this.head;
+        while (currentNode) {
+            listNode.push(currentNode);
+            currentNode = currentNode.next;
+        }
+        return listNode;
+    }
+
+    reverse() {
+        let currNode = this.head;
+        let prevNode = null;
+        let nextNode = null;
+        while (currNode) {
+            nextNode = currNode.next;
+            prevNode = currNode.previous;
+            currNode.next = prevNode;
+            currNode.previous = nextNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        this.tail = this.head;
+        this.head = prevNode;
+        return this;
+    }
 
  [Symbol.iterator]() {
         let current = this.head;
@@ -46,11 +115,20 @@ class DoublyLinkedList {
 
 }
  const a = new DoublyLinkedList;
+a.push(true);
 a.push(12);
 a.push(5);
 a.push('g');
 a.push(true);
+a.pop();
+a.push('ga-ga-ga');
+a.shift();
+a.unshift(55555);
 console.log(a);
 for(let key of a) {
     console.log(key);
 }
+const b = a.toArray();
+console.log(b);
+a.reverse();
+console.log(a);
