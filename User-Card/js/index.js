3f30aa0983cab25ea42ccfbc43caee3e8570ed7e
Anton-Pruglo <xatastudio@gmail.com>
2021-04-28 00:16:00 +0300

    Redact user-card

--- User-Card/js/index.js
+++ User-Card/js/index.js
@@ -2,18 +2,18 @@ const user = [
 	{
 		firstName: "Test",
 		lastName: "Testovich",
-		image: null,
+		image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500&quot;,ttps://www.kp.ru/share/i/12/10604075/ttps://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500&quot",
 	},
 	
 	{
 		firstName: "Ilon",
 		lastName: "Mask",
-		image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500&quot;,ttps://www.kp.ru/share/i/12/10604075/ttps://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500&quot",
+		image: "https://image.cnbcfm.com/api/v1/image/105439429-1536353186931screen-shot-2018-09-07-at-4.45.51-pm.jpg?v=1536353260&w=1600&h=900",
 	},
 	{
 		firstName: "Mirco",
 		lastName: "Kropovich",
-		image: null,
+		image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500&quot;,ttps://www.kp.ru/share/i/12/10604075/ttps://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500&quot",
 	},
 	{
 		firstName: "Jony",
@@ -25,10 +25,7 @@ const user = [
 		lastName: "Schwarzenegger",
 		image: "https://images-na.ssl-images-amazon.com/images/I/61u07onB8yL._AC_SX466_.jpg",
 	},
-]
-const arrImage = user.map(({firstName,lastName,image}) => image);
-const arrFullName = user.map(({firstName,lastName,image}) => `${firstName} ${lastName}`);
-
+];
 
 const userContainer = document.querySelector('.users');
 const template = document.querySelector('#clone-card');
@@ -37,29 +34,28 @@ const placeholder = template.content.querySelector('.image-placeholder');
 
 
 
-for(let key of arrFullName) {
-	h1.textContent = key;
-	placeholder.textContent = key.split(" ").map((n)=>n[0]).join("");
-	placeholder.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-	const copyCard = template.content.cloneNode(true);
-	userContainer.append(copyCard);
-	
-	}
-	
-for(let key of arrImage) {
+for(let key of user) {
 	const userCardImage = new Image();
-
 	userCardImage.addEventListener('load', ({currentTarget}) => {
 		const imageWrapper = document.querySelector('.image-wrapper');
-		
 		currentTarget.classList.add('image');
 		imageWrapper.innerHTML = '';
 		imageWrapper.appendChild(currentTarget);
-		
 	});
+	
+	
+	h1.textContent = `${key.firstName} ${key.lastName}`;
+	placeholder.textContent = `${key.firstName} ${key.lastName}`.split(" ").map((n)=>n[0]).join("");
+	placeholder.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+	const copyCard = template.content.cloneNode(true);
+	userContainer.append(copyCard);
+
+	
+	
 		
-	const imgSrc = key;	
-	userCardImage.src = imgSrc;	
+	userCardImage.src = key.image;
+	console.log(userCardImage.src);
+	
 }
 	
 
